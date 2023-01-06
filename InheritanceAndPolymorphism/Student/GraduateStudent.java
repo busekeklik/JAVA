a09d838f6b9c99fa290d36e3dd04be837453e07b
Buse Keklik <76210283+busekeklik@users.noreply.github.com>
2023-01-07 02:17:41 +0300

    Update GraduateStudent.java

--- InheritanceAndPolymorphism/Student/GraduateStudent.java
+++ InheritanceAndPolymorphism/Student/GraduateStudent.java
@@ -2,23 +2,11 @@ package Student;
 
 class GraduateStudent extends Student {
 
-//----------------------------------
-//    Constructors
-//----------------------------------
 
-    /**
-     * Default constructor
-     */
    public GraduateStudent( ) {
       super("No Name");
    }
-
-   /**
-     * Computes the course grade (Pass/No Pass) from the tests
-     * based on the criteria for the graduate
-     * students. Pass if avg >= 80; otherwise, No Pass.
-     *
-     */
+    
     public void computeCourseGrade() {
         int total = 0;
         for (int i = 0; i < NUM_OF_TESTS; i++) {
